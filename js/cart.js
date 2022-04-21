676aa91101fc616569feb3f916f1c623ab4a9047
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-04-21 15:34:56 +0200

    price...

--- js/cart.js
+++ js/cart.js
@@ -5,10 +5,16 @@ const cart = getCart();
 const container = document.querySelector(".product-container");
 
 if(cart.length === 0) {
-  container.innerHTML = "There are no favorites";
+  container.innerHTML = `<div class="error">
+                            <p>There are no favorites</p>
+                          </div>`
 }
 
+const totalPrice = document.querySelector(".total");
+let total = 0;
+
 cart.forEach((product) => {
+  total += product.price;
   container.innerHTML += `<div class="item">
                             <p>${product.title}</p>
                             <p>${product.price}</p>
@@ -16,4 +22,7 @@ cart.forEach((product) => {
                             <a href="details.html?id=${product.id}">
                             <p>Go to page</p></a>
                             </div>`
+                            
 });
+
+totalPrice.innerHTML = `Total: ${total}`;
